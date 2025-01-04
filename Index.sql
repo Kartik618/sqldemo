445552a70ee84a5305d7b6b4f61e684eb286fc88
kartik618 <kartik123gupta618@gmail.com>
2025-01-04 17:50:09 +0530

    insert syntax

--- Index.sql
+++ Index.sql
@@ -6,3 +6,6 @@ Use DemaSQL;
 --syntax
  CREATE TABLE <TABLE_NAME>(COLUN_NAME1 <DATATYPE>[SIZE], COLUMN_NAME2 <DATATYPE>[SIZE]
  ......... COLUMN_NAME_n<DATATYPE>[SIZE])
+
+ -- how to insert data into sql table ?
+ INSERT INTO  <TABLE_NAME> VALUES(VALUE_1,VALUE_2, VALUE_3............VALUE_n);
