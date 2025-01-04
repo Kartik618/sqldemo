e63a92a8a4bc79f0f817df3f4cca811ffa54d5b2
kartik618 <kartik123gupta618@gmail.com>
2025-01-04 17:45:43 +0530

    create table syntax

--- Index.sql
+++ Index.sql
@@ -2,3 +2,7 @@ create database DemoSQL;
 
 Use DemaSQL;
 
+-- how to create table in SQL server?
+--syntax
+ CREATE TABLE <TABLE_NAME>(COLUN_NAME1 <DATATYPE>[SIZE], COLUMN_NAME2 <DATATYPE>[SIZE]
+ ......... COLUMN_NAME_n<DATATYPE>[SIZE])
