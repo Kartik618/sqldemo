edc04ec32112129cdea17d3a49530b193ea3475a
kartik618 <kartik123gupta618@gmail.com>
2025-01-04 17:54:26 +0530

    update statement

--- Index.sql
+++ Index.sql
@@ -9,3 +9,10 @@ Use DemaSQL;
 
  -- how to insert data into sql table ?
  INSERT INTO  <TABLE_NAME> VALUES(VALUE_1,VALUE_2, VALUE_3............VALUE_n);
+
+ -- how to update the data in sql table ?
+
+ UPDATE <TABLE_NAME> 
+ SET COLUN_NAME1=VALUE_1,COLUMN_NAME2=VALUE_2, COLUMN_NAME3=VALUE_3.......
+     COLUMN_NAME_n=VALUE_n
+ WHERE COLUMN_NAME4=VALUE_4;    
